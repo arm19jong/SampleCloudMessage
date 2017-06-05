5753563a2ffa48a2c500a03254c03ddd2c0f0734
suchaj jongprasit <suchaj.jong@gmail.com>
2017-06-05 10:34:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jongzazaal/samplecloudmessage/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.jongzazaal.samplecloudmessage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "SampleCloudMessage";
+    private TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.txt);
+    }
+    public void showToken(View view) {
+        // แสดง token มาให้ดูหน่อยเสะ
+        mTextView.setText(FirebaseInstanceId.getInstance().getToken());
+        Log.i("token", FirebaseInstanceId.getInstance().getToken());
+    }
+    public void subscribe(View view) {
+        // สับตะไคร้หัวข้อ news
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        mTextView.setText(R.string.subscribed);
+    }
+    public void unsubscribe(View view) {
+        // ยกเลิกสับตะไคร้หัวข้อ news
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("news");
+        mTextView.setText(R.string.unsubscribed);
+    }
+
+}
